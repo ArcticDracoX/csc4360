06bdca9e1df98978f3cf372e9c5378ce4abdf876
ArcticDracoX'echo <kylechu2@gmail.com>
2024-09-24 16:53:25 -0400

    Initialise HW2

--- class_work_2/lib/home_screen.dart
+++ class_work_2/lib/home_screen.dart
@@ -1,61 +1,132 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
+
+void main()
+{
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
-// This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-// Application name
-title: 'Stateful Widget',
-theme: ThemeData(
-primarySwatch: Colors.blue,
-),
-// A widget that will be started on the application startup
-home: CounterWidget(),
-);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: _TabsNonScrollableDemo(),
+      ),
+    );
+  }
 }
+
+// Holds state for tab interface
+class _TabsNonScrollableDemo extends StatefulWidget {
+  @override
+  __TabsNonScrollableDemoState createState() => __TabsNonScrollableDemoState();
 }
-class CounterWidget extends StatefulWidget {
-@override
-_CounterWidgetState createState() => _CounterWidgetState();
-}
-class _CounterWidgetState extends State<CounterWidget> {
-//initial couter value
-int _counter = 0;
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: const Text('Stateful Widget'),
-),
-body: Column(
-mainAxisAlignment: MainAxisAlignment.center,
-children: [
-Center(
-child: Container(
-color: Colors.blue,
-child: Text(
-//displays the current number
-'$_counter',
-style: TextStyle(fontSize: 50.0),
-),
-),
-),
-Slider(
-min: 0,
-max: 100,
-value: _counter.toDouble(),
-onChanged: (double value) {
-setState(() {
-_counter = value.toInt();
-});
-},
-activeColor: Colors.blue,
-inactiveColor: Colors.red,
-),
-],
-),
-);
-}
+
+class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
+    with SingleTickerProviderStateMixin, RestorationMixin {
+  late TabController _tabController;
+
+  final RestorableInt tabIndex = RestorableInt(0);
+
+  @override
+  String get restorationId => 'tab_non_scrollable_demo';
+
+  // Restores last loaded state of tabs
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(tabIndex, 'tab_index');
+    _tabController.index = tabIndex.value;
+  }
+
+  // Initialises TabController
+  // Event listener
+  @override
+  void initState() {
+    super.initState();
+    // Creates 4 tabs
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 4,
+      vsync: this,
+    );
+    _tabController.addListener(() {
+      setState(() {
+        tabIndex.value = _tabController.index;
+      });
+    });
+  }
+
+  // Remover
+  @override
+  void dispose() {
+    _tabController.dispose();
+    tabIndex.dispose();
+    super.dispose();
+  }
+
+// Builds all basic structure
+  @override
+  Widget build(BuildContext context) {
+// For the ToDo task hint: consider defining the widget and name of the tabs here
+    final tabs = ['Text', 'Image', 'Button', 'List'];
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Tabs Demo',
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: false,
+          tabs: [
+            for (final tab in tabs) Tab(text: tab),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+// hint for the to do task:Considering creating the different for different tabs
+          //for (final tab in tabs)
+            // Text tab
+            Center(
+              child: Text("This is a test"),
+            ),
+            
+           // Image tab 
+            Center(
+	            child: Image.network(
+	              'https://ffxivcollect.com/assets/orchestrion-1715025e27527af41fc5daa8feb192d49887fa2c1b70b2f6fc6dcc74a5570109.png',
+	              width: 150,
+	              height: 150,
+	            ),
+            ),
+            
+            // Button tab
+            Center(
+	            child: ElevatedButton(
+	              onPressed: () {
+	                ScaffoldMessenger.of(context).showSnackBar(
+	                  SnackBar(
+	                    content: Text('Button pressed in ${tabs[2]} tab!'),
+	                  ),
+	                );
+	              },
+	              child: Text('Click me'),
+	            ),
+            ),
+
+            // List tab
+            ListView(
+              children: [
+                ListTile(title: Text('Item 1')),
+                ListTile(title: Text('Item 2')),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
 }
