cc2363b24d0e77a9bd9e625599e1433390482eee
ArcticDracoX'echo <kylechu2@gmail.com>
2024-11-07 17:43:24 -0500

    Initial class_activity_13

---
+++ class_activity_12/lib/inventory_item.dart
@@ -0,0 +1,13 @@
+class InventoryItem
+{
+  late int id;
+  late String amount;
+  late String item;
+
+  InventoryItem(
+  {
+    required this.id,
+    required this.amount,
+    required this.item,
+  });
+}
--- class_activity_12/lib/main.dart
+++ class_activity_12/lib/main.dart
@@ -1,56 +1,196 @@
-// Import Flutter package
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(InventoryApp());
+void main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
-class InventoryApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Inventory Management App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: InventoryHomePage(title: 'Inventory Home Page'),
+  Widget build(BuildContext context)
+  {
+    return const MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Test',
+      home: HomePage(),
     );
   }
 }
 
-class InventoryHomePage extends StatefulWidget {
-  InventoryHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class HomePage extends StatefulWidget
+{
+  const HomePage({super.key});
+
   @override
-  _InventoryHomePageState createState() => _InventoryHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _InventoryHomePageState extends State<InventoryHomePage> {
-// TODO: Implement Firestore integration
+class _HomePageState extends State<HomePage>
+{
+  // text fields' controllers
+  final TextEditingController amountController = TextEditingController();
+  final TextEditingController itemController = TextEditingController();
+  final CollectionReference inventory = FirebaseFirestore.instance.collection('Inventory');
+
+  Future<void> _createOrUpdate([DocumentSnapshot? documentSnapshot]) async
+  {
+    String action = 'create';
+    if (documentSnapshot != null)
+    {
+      action = 'update';
+      amountController.text = documentSnapshot['amount'].toString();
+      itemController.text = documentSnapshot['item'];
+    }
+
+    await showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext ctx)
+      {
+        return Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(ctx).viewInsets.bottom + 20,
+          ),
+
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:
+            [
+              TextField(
+                controller: amountController,
+                decoration: const InputDecoration(labelText: 'Amount'),
+              ),
+
+              TextField(
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                controller: itemController,
+                decoration: const InputDecoration(
+                  labelText: 'Item',
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              ElevatedButton(
+                child: Text(action == 'create' ? 'Create' : 'Update'),
+                onPressed: () async {
+                  int amount = int.parse(amountController.text);
+                  String item = itemController.text;
+                  if (amount != null && item.isNotEmpty)
+                  {
+                    if (action == 'create')
+                    {
+                      // Persist a newn itemController from inventory to Firestore
+                      await inventory.add({"amount": amount, "item": item});
+                    }
+
+                    if (action == 'update')
+                    {
+                      // Update then itemController from inventory
+                      await inventory.doc(documentSnapshot!.id).update({
+                        "amount": amount,
+                        "item": item,
+                      });
+                    }
+
+                    amountController.text = '';
+                    itemController.text = '';
+                    Navigator.of(context).pop();
+                  }
+                },
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Deleting an itemController from inventory by id
+  Future<void> _deleteProduct(String itemId) async
+  {
+    inventory.doc(itemId).delete;
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('You have successfully deleted an itemController from inventory'),
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('CRUD operations'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Inventory Management System',
-            ),
-// TODO: Implement inventory list view
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-// TODO: Implement add inventory item
+
+      // Using StreamBuilder to display all products from Firestore in real-time
+      body: StreamBuilder(
+        stream: inventory.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            return ListView.builder(
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index)
+              {
+                final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text(documentSnapshot['amountController'].toString()),
+                    subtitle: Text(documentSnapshot['itemController']),
+                    trailing: SizedBox(
+                      width: 100,
+                      child: Row(
+                        children:
+                        [
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () => _createOrUpdate(documentSnapshot),
+                          ),
+
+                          IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () => _deleteProduct(documentSnapshot.id),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
-        tooltip: 'Add Item',
-        child: Icon(Icons.add),
+      ),
+
+      // Add newn itemController from inventory
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _createOrUpdate(),
+        child: const Icon(Icons.add),
       ),
     );
   }
